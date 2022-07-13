fdb3a81b0c19109c99474c7a7540a49e1fbba424
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-07-13 09:43:23 +0300

    add task2

---
+++ lesson21/hw2/index.js
@@ -0,0 +1,16 @@
+export function getItemsList() {
+  const selectElements = document.querySelectorAll('.technology');
+  console.dir(selectElements);
+  return selectElements;
+}
+
+export function getItemsArray() {
+  const selectElements = document.querySelectorAll('.technology');
+  const selectElementsArr = Array.from(selectElements);
+  console.dir(selectElementsArr);
+  return selectElementsArr;
+}
+
+// test data
+getItemsList();
+getItemsArray();
