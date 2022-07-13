2b09ef367e74de3a552c2afa635da0a409068215
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-07-13 17:03:53 +0300

    4

--- lesson21/hw4/index.js
+++ lesson21/hw4/index.js
@@ -1,11 +1,11 @@
+export function setTitle(text) {
+  const findElement = document.querySelector('.title');
+  findElement.textContent = text;
+}
+
+// console.dir(setTitle('Hello bro'));
+
 // v.1
 // export function setTitle(text) {
 //   document.querySelector('.title').textContent = text;
 // }
-
-export function setTitle(text) {
-  const findElement = document.querySelector('.title');
-  findElement.textContent = `${text}`;
-}
-
-setTitle('Hello bro');
