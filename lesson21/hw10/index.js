37c58b6af8f8a9aadf8030fda8243d3bd8b424f9
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-07-13 23:33:24 +0300

    hw10

---
+++ lesson21/hw10/index.js
@@ -0,0 +1,10 @@
+export function manageClasses() {
+  document.querySelector('.one').classList.add('selected');
+  document.querySelector('.two').classList.remove('selected');
+  document.querySelector('.three').classList.toggle('three_done');
+  const fourElement = document.querySelector('.four');
+  if (fourElement.classList.contains('some-class')) {
+    fourElement.classList.add('another-class');
+  }
+}
+manageClasses();
