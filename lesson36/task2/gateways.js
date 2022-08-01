6145514b2f5adf5152007eefaa90e21787af44b8
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-08-02 00:19:40 +0300

    add task2 lesson36

---
+++ lesson36/task2/gateways.js
@@ -0,0 +1,17 @@
+export const fetchUserData = async userName => {
+  const response = await fetch(`https://api.github.com/users/${userName}`);
+  if (response.ok) {
+    const res = await response.json();
+    return res;
+  }
+  throw new Error('Failed to load data');
+};
+
+export const fetchRepositories = async url => {
+  const response = await fetch(url);
+  if (response.ok) {
+    const res = await response.json();
+    return res;
+  }
+  throw new Error('Failed to load data');
+};
---
+++ lesson36/task2/index.js
@@ -0,0 +1,49 @@
+import { fetchUserData, fetchRepositories } from './gateways.js';
+import { renderUserData } from './user.js';
+import { renderRepos, cleanReposList } from './repos.js';
+import { showSpinner, hideSpinner } from './spinner.js';
+
+const defaultUser = {
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+  name: '',
+  location: '',
+};
+
+renderUserData(defaultUser);
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+
+const onSearchUser = async () => {
+  showSpinner();
+  cleanReposList();
+  const userName = userNameInputElem.value;
+  try {
+    const userData = await fetchUserData(userName);
+    renderUserData(userData);
+    const reposList = await fetchRepositories(userData.repos_url);
+    renderRepos(reposList);
+  } catch (err) {
+    alert(err.message);
+  } finally {
+    hideSpinner();
+  }
+
+  // fetchUserData(userName)
+  //   .then(userData => {
+  //     renderUserData(userData);
+  //     return userData.repos_url;
+  //   })
+  //   .then(url => fetchRepositories(url))
+  //   .then(reposList => {
+  //     renderRepos(reposList);
+  //   })
+  //   .catch(err => {
+  //     alert(err.message);
+  //   })
+  //   .finally(() => {
+  //     hideSpinner();
+  //   });
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser);
